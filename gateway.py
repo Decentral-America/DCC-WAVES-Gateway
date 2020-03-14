fbd58a06746b4176d214f915175bb7b54387dc7f
iammortimer <info@mullerbeheer.nl>
2020-03-14 16:07:20 +0100

    update for disclaimer & api

--- gateway.py
+++ gateway.py
@@ -50,7 +50,8 @@ async def index(request: Request):
                                                      "wavesHeight": heights['Waves'],
                                                      "tnHeight": heights['TN'],
                                                      "tnAddress": config['tn']['gatewayAddress'],
-                                                     "wavesAddress": config['waves']['gatewayAddress']})
+                                                     "wavesAddress": config['waves']['gatewayAddress'],
+                                                     "disclaimer": config['main']['disclaimer']}})
 
 @app.get('/heights')
 async def getHeights():
@@ -77,3 +78,26 @@ async def getErrors(request: Request, username: str = Depends(get_current_userna
         dbCon = sqlite.connect('gateway.db')
         result = dbCon.cursor().execute('SELECT * FROM executed').fetchall()
         return templates.TemplateResponse("tx.html", {"request": request, "txs": result})
+
+@app.get("/api/fullinfo")
+async def api_fullinfo(request: Request):
+    heights = await getHeights()
+    return {"chainName": config['main']['name'],
+            "assetID": config['tn']['assetId'],
+            "tn_gateway_fee":config['tn']['gateway_fee'],
+            "tn_network_fee":config['tn']['network_fee'],
+            "tn_total_fee":config['tn']['network_fee']+config['tn']['gateway_fee'],
+            "waves_gateway_fee":config['waves']['gateway_fee'],
+            "waves_network_fee":config['waves']['network_fee'],
+            "waves_total_fee":config['waves']['network_fee'] + config['waves']['gateway_fee'],
+            "fee": config['tn']['fee'],
+            "company": config['main']['company'],
+            "email": config['main']['contact-email'],
+            "telegram": config['main']['contact-telegram'],
+            "recovery_amount":config['main']['recovery_amount'],
+            "recovery_fee":config['main']['recovery_fee'],
+            "wavesHeight": heights['Waves'],
+            "tnHeight": heights['TN'],
+            "tnAddress": config['tn']['gatewayAddress'],
+            "wavesAddress": config['waves']['gatewayAddress'],
+            "disclaimer": config['main']['disclaimer']}
